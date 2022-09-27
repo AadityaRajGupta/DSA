9f40eb30da64dd0203aea0497f334a7b84bd0d59
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2022-09-27 09:54:03 +0530

    jump search

--- DATA STRUCTURE LAB/lab4_searching4_jump.c
+++ DATA STRUCTURE LAB/lab4_searching4_jump.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 void print(int a[], int size)
 {
     printf("\nARRAY ELEMENT:\n");
@@ -21,20 +21,48 @@ int initialization(int a[])
     }
     return size;
 }
-
-int jump_searching(int a[], int key, int n)
-{
-    
-    return 0;
+int min(int a, int b){
+	if(b>a)
+	return a;
+	else
+	return b;
 }
+int jump_searching(int arr[], int x, int n)
+{
+	int step = sqrt(n);
 
+	int prev = 0;
+	while (arr[min(step, n)-1] < x)
+	{
+		prev = step;
+		step += sqrt(n);
+		if (prev >= n)
+			return -1;
+	}
+
+	while (arr[prev] < x)
+	{
+		prev++;
+		if (prev == min(step, n))
+			return -1;
+	}
+
+	if (arr[prev] == x)
+		return prev;
+
+	return -1;
+}
 int main()
 {
     int a[5], size;
     size = initialization(a);
     print(a, size);
 
-    jump_searching(a, 5, size);
+    int index = jump_searching(a, 5, size);
+	if(index >= 0)
+	printf("Number is at [%d] index",index);
+	else
+	printf("Number is not exist in the array");
 
     return 0;
 }
