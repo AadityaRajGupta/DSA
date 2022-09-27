53b688b8fe8fcd729e1b799944b3abb9c37578ce
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2022-09-27 18:06:52 +0530

    jump search complete

--- DATA STRUCTURE LAB/lab4_searching4_jump.c
+++ DATA STRUCTURE LAB/lab4_searching4_jump.c
@@ -21,36 +21,30 @@ int initialization(int a[])
     }
     return size;
 }
-int min(int a, int b){
-	if(b>a)
-	return a;
-	else
-	return b;
-}
-int jump_searching(int arr[], int x, int n)
+
+int jump_searching(int a[], int key, int size)
 {
-	int step = sqrt(n);
-
-	int prev = 0;
-	while (arr[min(step, n)-1] < x)
-	{
-		prev = step;
-		step += sqrt(n);
-		if (prev >= n)
-			return -1;
-	}
-
-	while (arr[prev] < x)
-	{
-		prev++;
-		if (prev == min(step, n))
-			return -1;
-	}
-
-	if (arr[prev] == x)
-		return prev;
-
-	return -1;
+    int step = sqrt(size);
+    int index = 0, i, j;
+    for (i = 0; i < size; i = i + step)
+    {
+        if (a[i] == key)
+        {
+            return i;
+        }
+        while (a[i] > key)
+        {
+            index = i - size + 1;
+            for (j = index; j < size; j++)
+            {
+                if (a[j] == key)
+                {
+                    return j;
+                }
+            }
+        }
+    }
+    return -1;
 }
 int main()
 {
@@ -58,11 +52,11 @@ int main()
     size = initialization(a);
     print(a, size);
 
-    int index = jump_searching(a, 5, size);
-	if(index >= 0)
-	printf("Number is at [%d] index",index);
-	else
-	printf("Number is not exist in the array");
+    int index = jump(a,5,size);//jump_searching(a, 5, size);
+    if (index >= 0)
+        printf("Number is at [%d] index", index);
+    else
+        printf("Number is not exist in the array");
 
     return 0;
 }
