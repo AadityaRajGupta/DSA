b1c12fdcac387f38934febc4745c08e82856f367
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2022-10-28 16:24:18 +0530

    selection sort complete

---
+++ DATA STRUCTURE LAB/lab7_sorting2_selection_sort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int initialize(int a[])
+{
+    int size,i;
+    printf("ENTER SIZE OF ARRAY:");
+    scanf("%d",&size);
+    printf("ENTER ARRAY ELEMENTS:\n");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    return size;
+}
+void print(int a[],int size)
+{
+    int i;
+    for(i=0;i<size;i++)
+    {
+        printf("%d  ",a[i]);
+    }
+}
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void selection_sort(int a[],int size)
+{
+    int i,j,min_index;
+    for(i=0;i<size;i++)
+    {
+        min_index=i;
+        for(j=i+1;j<size;j++)
+        {
+            if (a[j]<a[min_index])
+            {
+                min_index=j;
+            }
+        }
+        swap(&a[i],&a[min_index]);
+    }
+}
+int main()
+{
+    int a[10],size,i;
+    size=initialize(a);
+
+    printf("ARRAY BEFORE SORTING:\n");
+    print(a,size);
+
+    selection_sort(a,size);
+
+    printf("\n\nARRAY AFTER SORTING:\n");
+    print(a,size);
+
+    return 0;
+}
