1e2ea8de9da2c3d519cb2e2643a1ff32f4366066
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2022-12-01 22:14:05 +0530

    program lab 11 stack 4 complete

---
+++ DATA STRUCTURE LAB/lab11_stack4.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/*example:
+ENTER ANY INFIX EXPRESSION:A + B * C / D - F + A ^ E  
+PREFIX EXPRESSION: + ^ E A - F + / D * C B A
+
+
+ENTER ANY INFIX EXPRESSION:1 + 2 * 3 / 4 - 6 + 1 ^ 5 
+PREFIX EXPRESSION: + ^ 5 1 - 6 + / 4 * 3 2 1
+*/
+
+#define size 100
+
+#define create() s->top = -1;s->top_pre=-1
+#define push_A(item) s->symbols[++s->top] = item
+#define pop() s->symbols[s->top--]
+#define top_E() s->symbols[s->top]
+
+#define pop_Pre() s->prefix[s->top_pre--]
+#define push_Pre(item) s->prefix[++s->top_pre] = item
+
+struct stack
+{
+    char symbols[size];
+    char prefix[size];
+    int top_pre;
+    int top;
+} *s;
+
+int empty()
+{
+    while(s->top!=-1)
+        push_Pre(pop());
+}
+int display()
+{
+    printf("\nPREFIX EXPRESSION: ");
+    while(s->top_pre!=-1)
+        printf("%c ",pop_Pre());
+    printf("\n");
+}
+int main()
+{
+    // !Space separated values and one space at end compulsory
+    char str[size];
+    s=(struct stack *)malloc(sizeof(struct stack));
+    create();
+    printf("\n\nENTER ANY INFIX EXPRESSION:");
+    gets(str);
+
+    int flag=0,temp=0;
+
+    int i=-1;
+    // printf("\nPOSTFIX EXPRESSION: ");
+    while(str[++i]!='\0')
+    {
+        flag=0;
+        temp=0;
+        if (str[i]==' ')
+            continue;
+        else if (str[i]=='+')
+        {
+            while (top_E()=='+' || top_E()=='-' || top_E()=='*' || top_E()=='/' || top_E()=='^')
+                push_Pre(pop());
+            push_A(str[i]);
+        }
+        else if (str[i]=='-')
+        {
+            while (top_E()=='+' || top_E()=='-' || top_E()=='*' || top_E()=='/' || top_E()=='^')
+                push_Pre(pop());
+            push_A(str[i]);
+        }
+        else if (str[i]=='*')
+        {
+            while (top_E()=='*' || top_E()=='/' || top_E()=='^')
+                push_Pre(pop());
+            push_A(str[i]);
+        }
+        else if (str[i]=='/')
+        {
+            while (top_E()=='*' || top_E()=='/'|| top_E()=='^')
+                push_Pre(pop());
+            push_A(str[i]);
+        }
+        else if (str[i]=='^')
+            push_A(str[i]);
+        else
+            push_Pre(str[i]);
+    }
+    empty(); 
+    display();
+    return 0;
+}
