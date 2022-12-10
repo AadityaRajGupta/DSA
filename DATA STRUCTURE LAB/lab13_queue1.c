872158deaa30537db6dc658052aceeaaec4eef44
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2022-12-10 12:22:46 +0530

    program queue 1 complete

---
+++ DATA STRUCTURE LAB/lab13_queue1.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define size 5
+
+struct queue
+{
+    int a[size];
+    int f;
+    int r;
+}*q;
+
+int create()
+{
+    q->f=-1;
+    q->r=-1;
+}
+
+int isempty()
+{
+    if (q->f==q->r)
+    {
+        create();
+        return 1;
+    }
+    return 0;
+}
+
+int isfull()
+{
+    if(q->r==size-1)
+        return 1;
+    return 0;
+}
+int insertion(int item)
+{
+    if (!isfull())
+        q->a[++q->r]=item;
+    else
+        printf("\nQUEUE IS FULL!!!\n");
+}
+int deletion()
+{
+    if (!isempty())
+        printf("\nDELETED ELEMENT: %d",q->a[++q->f]);
+    else
+        printf("\nQUEUE IS EMPTY!!!");
+    return 0;
+}
+int display()
+{
+    if (!isempty())
+    {
+        printf("\n\nQUEUE:");
+        int i;
+        for (i= q->f + 1; i != q->r+1; i++)
+            printf("%d ", q->a[i]);
+    }
+    return 0;
+}
+int input()
+{
+    int item;
+    printf("\nENTER ANY NUMBER:");
+    scanf("%d",&item);
+    return item;
+}
+
+int main()
+{
+    int choose;
+    q=(struct queue *) malloc (sizeof(struct queue));
+    create();
+    while (1)
+    {
+        printf("\n1. INSERTION\n2. DELETION\n3. DISPLAY\n4. EXIT\nENTER YOUR CHOICE:");
+        scanf("%d",&choose);
+        switch(choose)
+        {
+            case 1:
+                insertion(input());
+                break;
+            case 2:
+                deletion();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("\n\nINVALID CHOICE!!!\n\n");
+                break;
+        }
+    }
+    return 0;
+}
