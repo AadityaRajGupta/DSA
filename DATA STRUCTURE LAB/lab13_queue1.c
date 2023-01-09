7bcb89b977a19053376cd0d84b57240b7bbad860
Aaditya Raj Gupta <aadityarajgupta333@gmail.com>
2023-01-09 18:15:21 +0530

    Array Implementaion of Linear Queue

--- DATA STRUCTURE LAB/lab13_queue1.c
+++ DATA STRUCTURE LAB/lab13_queue1.c
@@ -2,47 +2,53 @@
 #include <stdlib.h>
 
 #define size 5
+// Program of Array Implementaion of Linear Queue
+
+/*
+Queue follows the ⁡⁣⁣⁢First In First Out (FIFO)⁡ rule 
+
+In programming terms, putting items in the queue is called ⁡⁢⁢⁢ENQUEUE⁡,
+and removing items from the queue is called ⁡⁢⁢⁢DEQUEUE⁡.
+
+*/
 
 struct queue
 {
     int a[size];
     int f;
     int r;
-}*q;
-
+} *q;
 int create()
 {
-    q->f=-1;
-    q->r=-1;
+    q->f = -1;
+    q->r = -1;
 }
-
 int isempty()
 {
-    if (q->f==q->r)
+    if (q->f == q->r)
     {
         create();
         return 1;
     }
     return 0;
 }
-
 int isfull()
 {
-    if(q->r==size-1)
+    if (q->r == size - 1)
         return 1;
     return 0;
 }
 int insertion(int item)
 {
     if (!isfull())
-        q->a[++q->r]=item;
+        q->a[++q->r] = item;
     else
         printf("\nQUEUE IS FULL!!!\n");
 }
 int deletion()
 {
     if (!isempty())
-        printf("\nDELETED ELEMENT: %d",q->a[++q->f]);
+        printf("\nDELETED ELEMENT: %d", q->a[++q->f]);
     else
         printf("\nQUEUE IS EMPTY!!!");
     return 0;
@@ -53,7 +59,7 @@ int display()
     {
         printf("\n\nQUEUE:");
         int i;
-        for (i= q->f + 1; i != q->r+1; i++)
+        for (i = q->f + 1; i != q->r + 1; i++)
             printf("%d ", q->a[i]);
     }
     return 0;
@@ -62,35 +68,35 @@ int input()
 {
     int item;
     printf("\nENTER ANY NUMBER:");
-    scanf("%d",&item);
+    scanf("%d", &item);
     return item;
 }
 
 int main()
 {
     int choose;
-    q=(struct queue *) malloc (sizeof(struct queue));
+    q = (struct queue *)malloc(sizeof(struct queue));
     create();
     while (1)
     {
         printf("\n1. INSERTION\n2. DELETION\n3. DISPLAY\n4. EXIT\nENTER YOUR CHOICE:");
-        scanf("%d",&choose);
-        switch(choose)
+        scanf("%d", &choose);
+        switch (choose)
         {
-            case 1:
-                insertion(input());
-                break;
-            case 2:
-                deletion();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                return 0;
-            default:
-                printf("\n\nINVALID CHOICE!!!\n\n");
-                break;
+        case 1:
+            insertion(input());
+            break;
+        case 2:
+            deletion();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            return 0;
+        default:
+            printf("\n\nINVALID CHOICE!!!\n\n");
+            break;
         }
     }
     return 0;
